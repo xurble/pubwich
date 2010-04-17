9c36b50fe579c51d5fb8e62e2ef3313db958170c
Gareth Simpson <g@xurble.org>
2010-04-17 09:30:59 -0700

    Added tags to delicious. Updated main domain to point to delicious.com

--- lib/Services/Delicious.php
+++ lib/Services/Delicious.php
@@ -13,11 +13,42 @@
 	class Delicious extends RSS {
 
 		public function __construct( $config ){
-			$config['link'] = 'http://del.icio.us/'.$config['username'].'/';
+			$config['link'] = 'http://delicious.com/'.$config['username'].'/';
 			$config['url'] = sprintf( 'http://feeds.delicious.com/v2/rss/%s?count=%s', $config['username'], $config['total'] );
 			parent::__construct( $config );
-			$this->setItemTemplate('<li><a href="{%link%}">{%title%}</a></li>'."\n");
+			$this->setItemTemplate('<li class="link"><a href="{%link%}">{%title%}</a>{%tags%}</li>'."\n");
 		}
+
+		/**
+		 * @return array
+		 */
+		public function populateItemTemplate( &$item ) {
+			$comments_count = $item->children('http://purl.org/rss/1.0/modules/slash/')->comments;
+			$content = $item->children('http://purl.org/rss/1.0/modules/content/')->encoded;
+
+			$tags = ''; // pull tags out and make links of them
+			foreach($item->category as $t)
+			{
+				$tags .= '<li class="tag"><a href="'.$t->attributes().htmlspecialchars($t).'">'.htmlspecialchars($t).'</a></li>';
+			}
+			if($tags != '')
+			{
+				$tags = '<ul class="tags">'.$tags.'<li>Tagged:</li></ul>';
+			}
+
+			return array(
+						'link' => htmlspecialchars( $item->link ),
+						'title' => trim( $item->title ),
+						'date' => Pubwich::time_since( $item->pubDate ),
+						'comments_link' => $item->comments,
+						'comments_count' => $comments_count,
+						'description' => $item->description,
+						'content' => $content,
+						'author' => $item->author,
+						'tags' => $tags,
+			);
+		}
+
 
 	}
 
