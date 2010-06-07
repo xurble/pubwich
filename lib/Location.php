b707bb6376cf9ee6763224704f2731b7bc37e5e9
Gareth Simpson <g@xurble.org>
2010-06-06 22:25:03 -0700

    For regions, place the pin in the centroid, not in the first point.

--- lib/Location.php
+++ lib/Location.php
@@ -32,9 +32,51 @@
 			if ($c ==0)
 				return null;
 			else if ($c == 1)
-				return $this->points[0];
-			else 
-				return $this->points[0]; // need to do that fancy thing that gets the centre
+				$pt = $this->points[0];
+			else {
+				// This is a region, but I don't want regions
+				// find the centroid
+				//http://stackoverflow.com/questions/2792443/finding-the-centroid-of-a-polygon
+				
+				$pt = array(0,0);
+				$area = 0.0;
+				$x0 = 0.0;
+				$y0 = 0.0;
+				$x1 = 0.0;
+				$y1 = 0.0;
+				
+				for($i = 0;$i < $c-1;$i++) {
+					$x0 = $this->points[$i][0];
+					$y0 = $this->points[$i][1];
+					$x1 = $this->points[$i+1][0];
+					$y1 = $this->points[$i+1][1];
+					$a = ($x0 * $y1) - ($x1 * $y0);
+					$area  += $a;
+					$pt[0] += ($x0 + $x1) * $a;
+					$pt[1] += ($y0 + $y1) * $a;
+				}
+			
+
+				$x0 = $this->points[$i][0];
+				$y0 = $this->points[$i][1];
+				$x1 = $this->points[0][0];
+				$y1 = $this->points[0][1];
+				$a = ($x0 * $y1) - ($x1 * $y0);
+				$area  += $a;
+				$pt[0] += ($x0 + $x1) * $a;
+				$pt[1] += ($y0 + $y1) * $a;
+
+				$area *= 0.5;
+				$pt[0] /= (6 * $area);
+				$pt[1] /= (6 * $area);
+
+
+			}
+			
+			//	 I'd like to fuzz the location a little here
+			//	 to prevent flags being on top of each other
+			//	 but there's no stack overflow code to steal :)
+			return $pt;
 		
 		}
 		
